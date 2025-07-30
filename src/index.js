cf47af226198033f45a0ca46d0de9ce3a73e8f73
Hozaifa <hozaifataqui01@gmail.com>
2025-07-30 20:43:19 +0530

    completed basic sequlize setup for creating a DB

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 const express = require("express");
-require("dotenv").config(); // Load environment variables from .env file
+const bodyParser = require("body-parser");
+
 const {PORT} = require('./config/serverConfig'); // Import the PORT from serverConfig
 
 const SetupAndStartServer = async() => {
    // Create an instance of the express application or create the express object
     const app = express();
     // Set the port for the server to listen on
-    
+
+    app.use(bodyParser.json());
+    // Middleware to parse JSON bodies
+    app.use(bodyParser.urlencoded({ extended: true }));
+    // Middleware to parse URL encoded bodies
+
     app.listen(3000, () => {
         console.log(`Server is running on port ${PORT}`);
        
