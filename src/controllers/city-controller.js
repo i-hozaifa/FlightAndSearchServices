28e06d099a4898f713a49fefb2944c53eace62b8
Hozaifa <hozaifataqui01@gmail.com>
2025-08-26 17:01:48 +0530

    added city in controlller

---
+++ src/controllers/city-controller.js
@@ -0,0 +1,90 @@
+const {CityService} = require('../services/index');
+
+const cityService = new CityService();
+const create  = async (req, res) => {
+    try {
+       const city = await cityService.createCity(req.body);
+       return res.status(201).json({
+        data : city,
+        success : true,
+        message : "Successfully created a city",
+        err : {}
+       })
+    } catch (error) {
+        console.log("error in create city controller" );
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: "Not able to create a city",
+            err: error
+        }); 
+    }
+}
+
+const dellete  = async (req, res) => {
+    try {
+       const response = await cityService.deleteCity(req.params.id);
+       return res.status(200).json({
+        data : response,
+        success : true,
+        message : "Successfully delted a city",
+        err : {}
+       })
+    } catch (error) {
+        console.log("error in dellete city controller" );
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: "Not able to delete a city",
+            err: error
+        })
+    }
+}
+// GET --> /city/:id
+const get   = async (req, res) => {
+    try {
+       const response = await cityService.getCity(req.params.id);
+       return res.status(200).json({
+        data : response,
+        success : true,
+        message : "Successfully fetched  a city",
+        err : {}
+       });
+    } catch (error) {
+        console.log("error in catch city controller" );
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: "Not able to get a city",
+            err: error
+        });
+    }
+}
+//  PATCH --> /city/:id --> req.body
+const update  = async (req, res) => {
+    try {
+        const response = await cityService.updateCity(req.params.id);
+       return res.status(200).json({
+        data : response,
+        success : true,
+        message : "Successfully update  a city",
+        err : {}
+       });
+       
+    } catch (error) {
+        console.log("error in update city controller" );
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: "Not able to update a city",
+            err: error
+        });
+    }
+}
+
+module.exports = {
+    create,
+    dellete,
+    get,
+    update
+}
--- src/services/city-services.js
+++ src/services/city-services.js
@@ -1,1 +1,47 @@
-const {CityRepository} = require('../repository/index'); // Importing the CityRepository
+const {CityRepository} = require('../repository/index'); 
+// Importing the CityRepository 
+
+class CityService {
+    constructor(){
+    this.cityRepository = new CityRepository();
+}
+    async createCity(data) {
+        try {
+            const city = await this.cityRepository.CreateCity(data);
+        } catch (error) {
+            console.log("sometihng went wrong in service layer");
+            throw {error}; // Propagate the error to the caller
+        }
+    }
+
+    async deleteCity(cityId) {
+        try {
+            const repsonse = await this.cityRepository.deleteCity(cityId);
+        } catch (error) {
+             console.log("sometihng went wrong in service layer");
+            throw {error};
+        }
+    }
+
+    async updateCity(cityId, data) {
+        try {
+            const city = await this.cityRepository.updateCity(cityId, data);
+            retun city;
+        } catch (error) {
+             console.log("sometihng went wrong in service layer");
+            throw {error};
+        }
+    }
+
+    async getCity(cityId) {
+        try {
+            const city = await this.cityRepository.getCityById(cityId);
+            return city;
+        } catch (error) {
+             console.log("sometihng went wrong in service layer");
+            throw {error};
+        }
+    }
+}
+
+module.exports = CityService;
