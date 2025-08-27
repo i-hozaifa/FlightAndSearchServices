39b5a7f0fced70a1666c58bb0ee0f144cfa68db9
Hozaifa <hozaifataqui01@gmail.com>
2025-08-27 15:02:47 +0530

    adde mored routers

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,5 +4,7 @@ const CityController = require('../../controllers/city-controller'); // Importin
 const router = express.Router(); // Creating a new router instance
 
 router.post('/city', CityController.create); // Route to create a new city
-
+router.delete('/city/:id', CityController.dellete); // Route to delete a city by ID
+router.get('/city/:id', CityController.get); // Route to get a city by ID
+router.patch('/city/:id', CityController.update); // Route to update a city by ID
 module.exports = router; // Exporting the router to be used in other parts of the application
